6d7938674e7a9b6d30e8d29ca3630c3a598ed64c
TTTHF <thf_1995@163.com>
2019-03-28 23:16:45 +0800

    交换数组元素对

---
+++ 4-10/02/02.asm
@@ -0,0 +1,20 @@
+.386
+.model flat,stdcall
+.stack 4096
+
+
+.data
+array byte 10h,20h,30h,40h,50h,60h
+
+.code 
+main:
+	mov esi,OFFSET array
+	mov ecx,LENGTHOF array
+	L1:
+		mov al,[esi]
+		xchg al,[esi+1]
+		mov [esi],al
+		add esi,2
+		dec ecx   ;¶à¼õ1£¬³ý2Ð§¹û
+		loop L1
+end main
---
+++ 4-10/02/Bak/02(2).asm
@@ -0,0 +1,20 @@
+.386
+.model flat,stdcall
+.stack 4096
+
+
+.data
+array byte 10h,20h,30h,40h
+
+.code 
+main:
+	mov esi,OFFSET array
+	mov ecx,LENGTHOF array
+	L1:
+		mov al,[esi]
+		xchg al,[esi+1]
+		mov [esi],al
+		add esi,2
+		dec ecx
+		loop L1
+end main
